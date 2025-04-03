20d32fed182e3a524dfc950899016b2eb7d8ad17
irshadsulthani <154409684+irshadsulthani@users.noreply.github.com>
2025-04-03 10:26:51 +0530

    Time: 24 ms (23.93%), Space: 60.6 MB (15.64%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    let maxLength = 0;
+    let left = 0;
+    let count = {};
+
+    for (let right = 0; right < s.length; right++) {
+        let c = s[right];
+        count[c] = (count[c] || 0) + 1;
+        
+        while (count[c] > 1) {
+            count[s[left]] -= 1;
+            left++;
+        }
+        
+        maxLength = Math.max(maxLength, right - left + 1);
+    }
+    
+    return maxLength; 
+};
