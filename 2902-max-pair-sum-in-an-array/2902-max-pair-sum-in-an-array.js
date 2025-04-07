355051e9e435559faa6b672ff9a6e6d487021e03
irshadsulthani <154409684+irshadsulthani@users.noreply.github.com>
2025-04-07 09:48:54 +0530

    Time: 34 ms (31.25%), Space: 66.5 MB (5.21%) - LeetHub

--- 2902-max-pair-sum-in-an-array/2902-max-pair-sum-in-an-array.js
+++ 2902-max-pair-sum-in-an-array/2902-max-pair-sum-in-an-array.js
@@ -3,20 +3,32 @@
  * @return {number}
  */
 var maxSum = function(nums) {
-    let groups = {};
+    let groups = {}
 
-    for (let num of nums) {
-        let key = Math.max(...String(num));
-        (groups[key] ||= []).push(num);
+    for(let num of nums){
+        let key = Math.max(...String(num).split('').map(Number))
+        if(!groups[key]) groups[key] = []
+        groups[key].push(num)
     }
 
     return Math.max(
         ...Object.values(groups)
-            .map(g => g.sort((a, b) => b - a))
-            .filter(g => g.length > 1)
-            .map(g => g[0] + g[1]),
-        -1
-    );
+           .map((g) => g.sort((a,b)=> b - a ))
+           .filter(g => g.length > 1 )
+           .map(g => g[0] + g[1] ), 
+           -1
+    )
 };
+
+
+
+
+
+
+
+
+
+
+
 
 
