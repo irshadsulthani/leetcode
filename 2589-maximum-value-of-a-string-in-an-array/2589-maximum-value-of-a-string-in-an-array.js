d0ae513314b96b058707d375427146dc1cf952ff
irshadsulthani <154409684+irshadsulthani@users.noreply.github.com>
2025-05-05 11:23:37 +0530

    Time: 0 ms (100%), Space: 53.6 MB (59.5%) - LeetHub

--- 2589-maximum-value-of-a-string-in-an-array/2589-maximum-value-of-a-string-in-an-array.js
+++ 2589-maximum-value-of-a-string-in-an-array/2589-maximum-value-of-a-string-in-an-array.js
@@ -3,7 +3,20 @@
  * @return {number}
  */
 var maximumValue = function(strs) {
-    return Math.max(...strs.map(str => {
-        return !isNaN(str) ? parseInt(str, 10) : str.length;
-    }));
+    let max = 0;
+    for (const str of strs) {
+        let isNumber = true;
+        for (let i = 0; i < str.length; i++) {
+            if (str[i] < '0' || str[i] > '9') {
+                isNumber = false;
+                break;
+            }
+        }
+
+        const value = isNumber ? parseInt(str, 10) : str.length;
+        if (value > max) {
+            max = value;
+        }
+    }
+    return max;
 };
