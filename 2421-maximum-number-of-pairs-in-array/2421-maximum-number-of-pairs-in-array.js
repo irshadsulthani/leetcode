463079f20d9bd9b9b5348746f974180312cfed9f
irshadsulthani <154409684+irshadsulthani@users.noreply.github.com>
2025-05-05 10:56:46 +0530

    Time: 1 ms (64.25%), Space: 55 MB (24.64%) - LeetHub

--- 2421-maximum-number-of-pairs-in-array/2421-maximum-number-of-pairs-in-array.js
+++ 2421-maximum-number-of-pairs-in-array/2421-maximum-number-of-pairs-in-array.js
@@ -3,15 +3,15 @@
  * @return {number[]}
  */
 var numberOfPairs = function(nums) {
-    const freq = {};
-    let pairs = 0;
-    for (const num of nums) {
-        freq[num] = (freq[num] || 0) + 1;
-        
-        if (freq[num] % 2 === 0) {
-            pairs++;
+    const freq = {}
+    let pairs = 0
+    for(let num of nums){
+        freq[num] = (freq[num] || 0) + 1
+    
+    if(freq[num] % 2 === 0){
+        pairs++
         }
     }
-    const leftovers = nums.length - pairs * 2;
-    return [pairs, leftovers];
+    const left = nums.length - pairs * 2;
+    return [pairs, left]
 };
