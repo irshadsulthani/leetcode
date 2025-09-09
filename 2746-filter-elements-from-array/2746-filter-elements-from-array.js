8a83b21a0ee4d78ad9ad31d1d2891bbba775fdf3
irshadsulthani <154409684+irshadsulthani@users.noreply.github.com>
2025-09-09 16:52:22 +0530

    Time: 43 ms (49.45%), Space: 54.7 MB (9.74%) - LeetHub

--- 2746-filter-elements-from-array/2746-filter-elements-from-array.js
+++ 2746-filter-elements-from-array/2746-filter-elements-from-array.js
@@ -6,9 +6,7 @@
 var filter = function(arr, fn) {
     const result = [];
     for (let i = 0; i < arr.length; i++) {
-        if (fn(arr[i], i)) { 
-            result.push(arr[i]);
-        }
+        if (fn(arr[i], i)) result.push(arr[i]);
     }
     return result;
 };
